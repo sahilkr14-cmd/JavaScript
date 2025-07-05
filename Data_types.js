a5b917c751cffe77c9103559958225afba2be07f
sahilkr14-cmd <sahil5678sharma@gmail.com>
2025-07-05 14:14:00 +0530

    Data types.

---
+++ Data_types.js
@@ -0,0 +1,14 @@
+
+let username = "Bro";         // String
+let score = 99;               // Number
+let isPassed = true;          // Boolean
+let nothing = null;           // Null
+let notSet;                   // Undefined
+
+let user = { name: "Bro", age: 20 }; // Object
+let numbers = [1, 2, 3];           // Array
+
+console.log(typeof username);  // string
+console.log(typeof score);     // number
+console.log( user);  
+console.log( numbers);     
