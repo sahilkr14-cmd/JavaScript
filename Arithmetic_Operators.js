662ceda7a55518ad9d0702113d10ea473d776f48
sahilkr14-cmd <sahil5678sharma@gmail.com>
2025-07-05 14:16:56 +0530

    Arithmetic Operators

---
+++ Arithmetic_Operators.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 3;
+
+console.log("Addition:", a + b);         
+console.log("Subtraction:", a - b);      
+console.log("Multiplication:", a * b);   
+console.log("Division:", a / b);         
+console.log("Modulus:", a % b);          
+console.log("Power:", a ** b);  
