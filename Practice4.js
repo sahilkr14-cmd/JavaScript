4f9bb6c62717ab93ff7628645796d68904b8d2c2
sahilkr14-cmd <sahil5678sharma@gmail.com>
2025-07-07 02:24:40 +0530

    Solving Practice Questions

---
+++ Practice4.js
@@ -0,0 +1,16 @@
+let age = 18
+
+if (age < 13) {
+    console.log(" Child ")
+}
+
+else if (age >= 13 && age <= 19) {
+    console.log(" Teenager ")
+}
+
+else if (age >= 20 && age <=59) {
+    console.log(" Adult ")
+}
+
+else
+    console.log(" Senior Citizen ")
