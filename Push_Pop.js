b709eb3e3789f2cd479594c30f2b9218bbaed97a
sahilkr14-cmd <sahil5678sharma@gmail.com>
2025-07-20 03:48:17 +0530

    Push Pop method on Array

---
+++ Push_Pop.js
@@ -0,0 +1,10 @@
+let nums = [10, 20, 30];
+nums.push(40);     // [10, 20, 30, 40]
+// nums.pop();        // [10, 20, 30]
+console.log(nums.length);  // 4
+
+
+let num = [10, 20, 30];
+num.push(40);     // [10, 20, 30, 40]
+num.pop();        // [10, 20, 30]
+console.log(num.length);  // 3
