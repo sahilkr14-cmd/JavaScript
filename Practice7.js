535bf92adc33ca2058d0e76fa15d8d89cdf43e41
sahilkr14-cmd <sahil5678sharma@gmail.com>
2025-07-24 20:19:30 +0530

    Using Push, Pop, Includes and Indexof Methods

---
+++ Practice7.js
@@ -0,0 +1,8 @@
+let colors = ["Red ","Green ", "Orange"]
+colors.push("Yellow")
+console.log(colors)
+colors.pop()
+console.log(colors)
+console.log(colors.includes("Red "))
+console.log(colors.indexOf("Orange")
+)
